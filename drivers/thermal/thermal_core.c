6a52998b5371d6d4c6ad9107f262334a8c2f1405
Vaisakh Murali <mvaisakh@statixos.com>
2021-06-02 11:49:39 +0530

    drivers: thermal: Drop unused functions
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1693,60 +1693,6 @@ static int update_thermal_target(struct thermal_zone_device *tz,
 	return ret;
 }
 
-static int pa1_mmw0_thermal_qos_handler(struct notifier_block *b, unsigned long val, void *v)
-{
-	return update_thermal_target(pa1_mmw0_tz, val, 0);
-}
-
-static struct notifier_block pa1_mmw0_thermal_qos_notifier = {
-	.notifier_call = pa1_mmw0_thermal_qos_handler,
-};
-
-static int xo_mmw1_thermal_qos_handler(struct notifier_block *b, unsigned long val, void *v)
-{
-	return update_thermal_target(xo_mmw1_tz, val, 0);
-}
-
-static struct notifier_block xo_mmw1_thermal_qos_notifier = {
-	.notifier_call = xo_mmw1_thermal_qos_handler,
-};
-
-static int modem_skin_thermal_qos_handler(struct notifier_block *b, unsigned long val, void *v)
-{
-	return update_thermal_target(modem_skin_tz, val, 0);
-}
-
-static struct notifier_block modem_skin_thermal_qos_notifier = {
-	.notifier_call = modem_skin_thermal_qos_handler,
-};
-
-static int modem_mmw2_qos_handler(struct notifier_block *b, unsigned long val, void *v)
-{
-	return update_thermal_target(modem_mmw2_tz, val, 0);
-}
-
-static struct notifier_block modem_mmw2_qos_notifier = {
-	.notifier_call = modem_mmw2_qos_handler,
-};
-
-static int msm_thermal_qos_handler(struct notifier_block *b, unsigned long val, void *v)
-{
-	return update_thermal_target(msm_tz, val, 0);
-}
-
-static struct notifier_block msm_thermal_qos_notifier = {
-	.notifier_call = msm_thermal_qos_handler,
-};
-
-static int skin_thermal_qos_handler(struct notifier_block *b, unsigned long val, void *v)
-{
-	return update_thermal_target(skin_tz, val, 1);
-}
-
-static struct notifier_block skin_thermal_qos_notifier = {
-	.notifier_call = skin_thermal_qos_handler,
-};
-
 static int __init thermal_init(void)
 {
 	int result;
